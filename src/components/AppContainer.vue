559d5b0564d1d9f6f4c6a1bbadbfd1f5c2164685
AShikShuvo <ashikshuvo1996@gmail.com>
2023-10-03 18:57:07 +0600

    page layout

---
+++ src/components/AppContainer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col justify-center items-center h-screen w-screen">
+    <div class="h-3/4 w-4/5 border-2 border-sky-500 p-4">
+        <div class="h-full w-full grid grid-cols-3 gap-3.5">
+          <div class="border-2 border-sky-300 p-4 col-span-2">left</div>
+          <div class="border-2 border-sky-300 p-4">right</div>
+        </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
